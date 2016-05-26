c7ece561a617db4fd32dc48361a36e0eef2b5946
Stuart Hall <stuart22@gmail.com>
2016-05-26 01:16:11 -0400

    Week 1 commit

---
+++ Week1/test/training_day_test.clj
@@ -0,0 +1,21 @@
+(ns iloveponies.tests.training-day
+  (:use midje.sweet
+        training-day))
+
+(facts "answer" {:exercise 5
+                 :points 1}
+  answer => 42)
+
+(facts "hai" {:exercise 5
+               :points 1}
+  hai => "OH HAI!")
+
+(facts "square" {:exercise 7
+                 :points 1}
+  (square 2) => 4
+  (square 3) => 9)
+
+(facts "average" {:exercise 8
+                  :points 1}
+  (average 2 4) => 3
+  (average 1 2) => 3/2)
