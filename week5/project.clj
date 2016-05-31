5d58c232506d383f4a5e8550a9b43b52166f28b9
Stuart Hall <stuart22@gmail.com>
2016-05-31 01:50:13 -0400

    Week 5 Upload

---
+++ week5/project.clj
@@ -0,0 +1,5 @@
+(defproject predicates "1.0.0-SNAPSHOT"
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [midje "1.8.3"]
+                 [midje-grader "0.1.0-SNAPSHOT"]]
+  :profiles {:dev {:plugins [[lein-midje "3.1.1"]]}})
